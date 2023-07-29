fc2dbc10a78fdad9181afcf2fa0739d6d70ddda6
Oussa-Err <dexterlecter0@gmail.com>
2023-07-30 00:51:09 +0100

    header added

--- front-end/tsf-foundation/src/app/component/Header.jsx
+++ front-end/tsf-foundation/src/app/component/Header.jsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className="flex bg-transparent h-24 items-center">
+        <div className="flex-1 pl-2">
+          <Link href="/" className="text-2xl font-extrabold ">AFRICA DIGITAL FARMING SCHOOL</Link>
+        </div>
+        <nav className="flex-1 flex justify-around">
+          <Link href="/"> Acceuil</Link>
+          <Link href="/adfSchool"> ADF school</Link>
+          <Link href="/formation"> Formation</Link>
+          <Link href="/contact"> Contact</Link>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
